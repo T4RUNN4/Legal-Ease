c76feb6c0d58e98cf6a6a75c174e7d80df3e96fe
Tarunna Sen <tarunnasen@gmail.com>
2026-06-18 22:45:47 +0600

    Dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,15 @@
 export default function Dashboard() {
-    return (
-        <p>Hello World!</p>
-    )
+  return (
+    <div className="p-4 text-[#43311c] flex gap-10 mt-10">
+      <img
+        src="https://i.pinimg.com/736x/ab/57/ca/ab57cadd895944460c54e562c50d352e.jpg"
+        alt="Portrait of Alexander Reed"
+        className="w-full h-full object-cover"
+      />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h3 className="text-xl font-medium mb-1">Alexander Reed</h3>
+        <p>Joined: 15th April, 2026</p>
+      </div>
+    </div>
+  );
 }
