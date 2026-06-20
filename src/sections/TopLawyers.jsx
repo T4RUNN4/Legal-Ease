18b902aa5c849e68d60edc02fc659fb95c434d67
Tarunna Sen <tarunnasen@gmail.com>
2026-06-20 16:32:55 +0600

    Removed console.log

--- src/sections/TopLawyers.jsx
+++ src/sections/TopLawyers.jsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 export default async function TopLawyer() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/lawyers/top`);
   const lawyers = await res.json();
-  console.log(lawyers)
 
   return (
     <section className="py-24 px-6 md:px-12 lg:px-24">
