f879beb4c960036d65bbb8e4e066f56da61488b9
Tarunna Sen <tarunnasen@gmail.com>
2026-06-20 14:08:36 +0600

    Transaction History

--- src/app/dashboard/admin/transactions/page.js
+++ src/app/dashboard/admin/transactions/page.js
@@ -1,30 +1,10 @@
-export default function Transactions() {
-  const transactions = [
-    {
-      id: 1,
-      transactionId: "TXN001",
-      lawyer: "John Doe",
-      email: "john.doe@example.com",
-      amount: "$100",
-      date: "2023-08-01",
-    },
-    {
-      id: 2,
-      transactionId: "TXN002",
-      lawyer: "Jane Smith",
-      email: "jane.smith@example.com",
-      amount: "$200",
-      date: "2023-08-02",
-    },
-    {
-      id: 3,
-      transactionId: "TXN003",
-      lawyer: "Bob Johnson",
-      email: "bob.johnson@example.com",
-      amount: "$300",
-      date: "2023-08-03",
-    },
-  ];
+import { format } from "date-fns";
+
+export default async function Transactions() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/transactions`,
+  );
+  const transactions = await res.json();
 
   return (
     <section className="space-y-6 mt-10 flex flex-col items-center">
@@ -33,10 +13,12 @@ export default function Transactions() {
       </div>
 
       <div className="border border-white/10 mt-8 w-full">
-        <table className="table w-full rounded-none text-left">
+        <table className="table w-full rounded-none text-center">
           <thead>
             <tr className="border-b border-white/10 uppercase tracking-wider text-xl">
-              <th className="py-4 px-6 rounded-none font-medium">Transaction ID</th>
+              <th className="py-4 px-6 rounded-none font-medium">
+                Transaction ID
+              </th>
               <th className="py-4 px-6 font-medium">Lawyer</th>
               <th className="py-4 px-6 font-medium">Amount</th>
               <th className="py-4 px-6 font-medium">Date</th>
@@ -44,11 +26,16 @@ export default function Transactions() {
           </thead>
           <tbody className="divide-y divide-white/5 text-lg">
             {transactions.map((transaction) => (
-              <tr key={transaction.id} className="hover:bg-white/5 transition-colors">
-                <td className="py-4 px-6 font-medium">{transaction.transactionId}</td>
-                <td className="py-4 px-6">{transaction.lawyer}</td>
-                <td className="py-4 px-6">{transaction.amount}</td>
-                <td className="py-4 px-6">{transaction.date}</td>
+              <tr
+                key={transaction.id}
+                className="hover:bg-white/5 transition-colors"
+              >
+                <td className="py-4 px-6 font-medium">
+                  {transaction.transaction.transactionId}
+                </td>
+                <td className="py-4 px-6">{transaction.lawyerName}</td>
+                <td className="py-4 px-6">${transaction.fee}</td>
+                <td className="py-4 px-6"> {format(new Date(transaction.transaction.transactionDate), "PPPP")}</td>
               </tr>
             ))}
           </tbody>
