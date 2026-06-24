1d28295c7cce5edcdb1da93b85b9ebaf548e4145
Tarunna Sen <tarunnasen@gmail.com>
2026-06-24 16:40:12 +0600

    Improved UX - Payment cancel page

--- src/app/checkout/cancel/page.js
+++ src/app/checkout/cancel/page.js
@@ -9,14 +9,13 @@ export default function Cancel() {
         <SubHeading text="Payment Failed" />
         <Heading texts={["Payment Cancellation"]} />
 
-        <p className="text-xl mt-16">
+        <p className="text-xl mt-16 text-rose-600">
           Your payment process is either cancelled or failed due to some
           reasons. Please try again later.
         </p>
 
-        <div className="flex gap-4 mt-6">
-          <Button text="Go To Home" link="/" variant="dark" />
-          <Button text="Dashbaord" link="dashboard" />
+        <div className="mt-6">
+          <Button text="Go To Dashbaord" link="/dashboard" variant="dark" />
         </div>
       </SectionStructure>
     );
