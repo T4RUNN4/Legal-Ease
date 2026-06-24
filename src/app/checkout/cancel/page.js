e629f7167b2b21de2951358db87ee847f13cd398
Tarunna Sen <tarunnasen@gmail.com>
2026-06-24 16:34:32 +0600

    Improved component - Payment Cancellation

--- src/app/checkout/cancel/page.js
+++ src/app/checkout/cancel/page.js
@@ -1,3 +1,23 @@
+import Button from "@/components/Button";
+import Heading from "@/components/Heading";
+import SectionStructure from "@/components/SectionStructure";
+import SubHeading from "@/components/SubHeading";
+
 export default function Cancel() {
-    return <p>Payment Failed!</p>
+    return (
+      <SectionStructure>
+        <SubHeading text="Payment Failed" />
+        <Heading texts={["Payment Cancellation"]} />
+
+        <p className="text-xl mt-16">
+          Your payment process is either cancelled or failed due to some
+          reasons. Please try again later.
+        </p>
+
+        <div className="flex gap-4 mt-6">
+          <Button text="Go To Home" link="/" variant="dark" />
+          <Button text="Dashbaord" link="dashboard" />
+        </div>
+      </SectionStructure>
+    );
 }
